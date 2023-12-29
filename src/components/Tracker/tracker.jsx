2d689716f774038b869f920f222d967c77a703cf
Kristian <krisplakcoder@gmail.com>
2023-12-29 03:41:10 -0800

    added chapter counter to the main page

---
+++ src/components/Tracker/tracker.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import bookdata from "../../assets/data/bookdata.json"
+import "./tracker.scss"
+
+export default function Tracker() {
+
+    const [currentChapter, setCurrentChapter] = useState(1);
+
+    function reduceChapterCount() {
+        if (currentChapter > 1) {
+            setCurrentChapter(currentChapter - 1);
+        }
+    }
+
+    function increaseChapterCount() {
+        if (currentChapter < bookdata.chapters) {
+            setCurrentChapter(currentChapter + 1);
+        }
+    }
+
+
+    return (
+        <>
+            <p>The book title is: {bookdata.title}</p>
+            <img src={bookdata.image} alt="current book cover" className="tracker-image" />
+            <div className="buttons">
+                <button className="minus" onClick={reduceChapterCount}>-</button>
+                <button className="add" onClick={increaseChapterCount}>+</button>
+            </div>
+            <div className="chapter">
+                {currentChapter}/{bookdata.chapters}
+            </div>
+            <h1>Tracker goes here!</h1>
+        </>
+    )
+}
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/header";
+import Tracker from "../../components/Tracker/tracker";
 
 export default function HomePage() {
     return (
         <>
             <Header />
+            <Tracker />
             <h1>I am a HomePage</h1>
         </>
     )
