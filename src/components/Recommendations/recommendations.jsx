6b7b8e8f5078cf578270af1bd690c5e544f6c149
Kristian <krisplakcoder@gmail.com>
2024-01-01 03:24:05 -0800

    developed a container for the recommended card list

--- src/components/Recommendations/recommendations.jsx
+++ src/components/Recommendations/recommendations.jsx
@@ -11,30 +11,32 @@ export default function Recommendations() {
     return (
         <>
             <section className="recommendations-section">
-                <div className="recommendations__card">
-                    <div className="recommendations__buttons--close">
-                        <button className="close">X</button>
-                    </div>
-                    <div className="recommendations__book">
-                        <div className="recommendations__book-cover">
-                            <img src={bookdata.image} alt="book cover" className="recommendations__book-cover-image" />
+                <ul className="recommendations__card-container">
+                    <li className="recommendations__card">
+                        <div className="recommendations__buttons--close">
+                            <button className="close">X</button>
                         </div>
-                        <div className="recommendations__book-details">
-                            <div className="recommendations__book-details-title">
-                                <span className="recommendations__book-details--bold">{bookdata.title}</span>
+                        <div className="recommendations__book">
+                            <div className="recommendations__book-cover">
+                                <img src={bookdata.image} alt="book cover" className="recommendations__book-cover-image" />
                             </div>
-                            <div className="recommendations__book-details-author">
-                                <span className="recommendations__book-details--bold">{bookdata.author}</span>
-                            </div>
-                            <div className="recommendations__book-details-date">
-                                <span className="recommendations__book-details-title--bold">{bookdata.publishedDate}</span>
-                            </div>
-                            <div className="recommendations__search-button">
-                                <button className="recommendations__buttons-find" onClick={findBook}>Find</button>
+                            <div className="recommendations__book-details">
+                                <div className="recommendations__book-details-title">
+                                    <span className="recommendations__book-details--bold">{bookdata.title}</span>
+                                </div>
+                                <div className="recommendations__book-details-author">
+                                    <span className="recommendations__book-details--bold">{bookdata.author}</span>
+                                </div>
+                                <div className="recommendations__book-details-date">
+                                    <span className="recommendations__book-details-title--bold">{bookdata.publishedDate}</span>
+                                </div>
+                                <div className="recommendations__search-button">
+                                    <button className="recommendations__buttons-find" onClick={findBook}>Find</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </li>
+                </ul>
             </section>
         </>
     )
