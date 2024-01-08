e11189b8ad53397f1ea30e4c9c655a3896db603a
Kristian <krisplakcoder@gmail.com>
2024-01-07 20:48:44 -0800

    completed the completed book div function

--- src/components/Tracker/trackerButtons.jsx
+++ src/components/Tracker/trackerButtons.jsx
@@ -40,31 +40,28 @@ export default function TrackerButtons({page, totalPages, id}) {
         }
     }
 
-  
-     // useEffect(() => {
-    //     const checkTracker = () => {
-    //         console.log(newPage, totalPages);
-    //         if (newPage === totalPages || page === totalPages) {
-    //             setTrackerState(
-    //             <div className="tracker-bar--complete">
-    //                 <div className="tracker-bar--complete-message"><h3>Completed!!!</h3></div>
-    //             </div>)
-    //         } else {
-    //             setTrackerState(
-    //              <div className="tracker-bar">
-    //                 <label htmlFor="tracker-bar" className="tracker-chapter">Page: {newPage}/{totalPages}</label>
-    //                 <progress id="tracker-bar" className="tracker-bar-progress" value = {newPage} max={totalPages} />
-    //             </div>)
-    //         }
-    //     }; checkTracker();
-    // },[newPage])
+    
+     useEffect(() => {
+        const checkTracker = () => {
+            console.log(newPage, totalPages);
+            if (newPage === totalPages || page === totalPages) {
+                setTrackerState(
+                <div className="tracker-bar--complete">
+                    <div className="tracker-bar--complete-message"><h3>Completed!!!</h3></div>
+                </div>)
+            } else {
+                setTrackerState(
+                 <div className="tracker-bar">
+                    <label htmlFor="tracker-bar" className="tracker-chapter">Page: {newPage}/{totalPages}</label>
+                    <progress id="tracker-bar" className="tracker-bar-progress" value = {newPage} max={totalPages} />
+                </div>)
+            }
+        }; checkTracker();
+    },[newPage])
 
     return (
         <>
-            <div className="tracker-bar">
-                <label htmlFor="tracker-bar" className="tracker-chapter">Page: {newPage}/{totalPages}</label>
-                <progress id="tracker-bar" className="tracker-bar-progress" value = {newPage} max={totalPages} />
-            </div>
+            {trackerState}
             <div className="tracker-buttons">
                 <button className="tracker-button tracker-buttons--minus" onClick={reduceChapterCount}>-</button>
                 <button className="tracker-button tracker-buttons--add" onClick={increaseChapterCount}>+</button>
